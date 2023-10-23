338e84c56967b1d0e9335f03528808db3c15c8cc
Robel <robelisaisalgado3@gmail.com>
2023-10-23 15:40:44 -0600

    testtunner1

--- data/scripts/bsSpaz.py
+++ data/scripts/bsSpaz.py
@@ -1090,8 +1090,8 @@ class Spaz(bs.Actor):
                 g = bs.getGameTime()
                 def charChange(val):
                     if val:
-                        t.colorTexture = bs.getTexture(tunners+'Color')
-                        t.colorMaskTexture = bs.getTexture(tunners+'ColorMask')
+                        t.colorTexture = bs.getTexture(tunners)
+                        t.colorMaskTexture = bs.getTexture(tunners)
                 charChange(True)
                
             self.node.handleMessage("flash")
